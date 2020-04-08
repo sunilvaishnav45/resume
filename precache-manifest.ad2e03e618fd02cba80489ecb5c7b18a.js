e5ffd52c9ba4c2d8f4e5158d67d59858479ccf2c
Sunil <sunil@Sunils-MacBook-Air.local>
2020-04-08 12:48:20 +0530

    Updates

---
+++ precache-manifest.ad2e03e618fd02cba80489ecb5c7b18a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc9e07b88f84e136b66b694c822e86cf",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "51165d6940bc678ff1db",
+    "url": "/resume/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "bfe7434eedaf0366b202",
+    "url": "/resume/static/css/main.722f8ef6.chunk.css"
+  },
+  {
+    "revision": "51165d6940bc678ff1db",
+    "url": "/resume/static/js/2.0d06f9cc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/resume/static/js/2.0d06f9cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfe7434eedaf0366b202",
+    "url": "/resume/static/js/main.59a11394.chunk.js"
+  },
+  {
+    "revision": "09dbdedc89bdf34a4aae",
+    "url": "/resume/static/js/runtime-main.adcd7a6d.js"
+  },
+  {
+    "revision": "aad8841693cf905c5ec15478f7bdea5a",
+    "url": "/resume/static/media/sunil.aad88416.jpeg"
+  }
+]);
