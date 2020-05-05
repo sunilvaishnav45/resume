16fe65fb2d00ee8dad035e5c87a8f7423449d536
Sunil <sunil@Sunils-MacBook-Air.local>
2020-05-05 17:56:19 +0530

    Footer updated

--- src/Components/Page1/page1.jsx
+++ src/Components/Page1/page1.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Profile from '../Profile/profile';
 import WorkExperince from '../WorkExperince/workexperince';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCaretRight, faCaretLeft } from '@fortawesome/free-solid-svg-icons';
 
 class Page1 extends React.Component {
 
@@ -21,8 +23,8 @@ class Page1 extends React.Component {
                 <Profile />
                 <WorkExperince />
                 <div className="text-center mt-2">
-                    <Link to="/resume/profile-experince"> <div className="dot mr-3 active"></div> </Link>
-                    <Link to="/resume/education-skills-git">    <div className="dot "></div> </Link>
+                    <Link to="/resume/profile-experince"> <div className="footer-dot mr-3 active"><span className="text-white"><FontAwesomeIcon icon={faCaretLeft} /></span></div> </Link>
+                    <Link to="/resume/education-skills-git"> <div className="footer-dot "><span className="text-white"><FontAwesomeIcon icon={faCaretRight} /></span></div> </Link>
                 </div>
             </React.Fragment>
         );
--- src/Components/Page2/page2.jsx
+++ src/Components/Page2/page2.jsx
@@ -5,6 +5,8 @@ import Education from '../Education/education';
 import Skills from '../Skills/skills';
 import Git from '../Git/git';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCaretRight, faCaretLeft } from '@fortawesome/free-solid-svg-icons';
 
 class Page2 extends React.Component {
 
@@ -25,8 +27,8 @@ class Page2 extends React.Component {
                 <Skills />
                 <Git />
                 <div className="text-center mt-2">
-                    <Link to="/resume/profile-experince"> <div className="dot mr-3 "></div> </Link>
-                    <Link to="/resume/education-skills-git"> <div className="dot active"></div> </Link>
+                    <Link to="/resume/profile-experince"> <div className="footer-dot mr-3 "><span className="text-white"><FontAwesomeIcon icon={faCaretLeft} /></span></div> </Link>
+                    <Link to="/resume/education-skills-git"> <div className="footer-dot active"><span className="text-white"><FontAwesomeIcon icon={faCaretRight} /></span></div> </Link>
                 </div>
             </React.Fragment>
         );
