86a045a23a45becfa591d81cb3a1a173cf9b6cf4
sunilvaishnav45 <sunilvaishnav45@gmail.com>
2024-07-18 19:46:10 +0530

    Updated resumes added

--- src/Components/Page1/page1.jsx
+++ src/Components/Page1/page1.jsx
@@ -20,12 +20,12 @@ class Page1 extends React.Component {
     render() {
         return (
             <React.Fragment>
+                <Skills />
                 <Profile />
                 <WorkExperince />
-                <div className="text-center mt-2">
-                    <Link to="/resume/profile-experince"> <div className="footer-dot mr-3 active"><span className="text-white"><FontAwesomeIcon icon={faCaretLeft} /></span></div> </Link>
-                    <Link to="/resume/education-skills-git"> <div className="footer-dot "><span className="text-white"><FontAwesomeIcon icon={faCaretRight} /></span></div> </Link>
-                </div>
+                <Education />
+                <Skills />
+                <Git />
             </React.Fragment>
         );
     }
